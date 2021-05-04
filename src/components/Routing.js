d0de6c6390dc7be11ff359cda89968a98b92823a
marcinbodnardesktop <me@marcinbodnar.com>
2021-05-05 00:12:56 +0200

    Profile handles empty account state (#1665)

--- src/components/Routing.js
+++ src/components/Routing.js
@@ -317,7 +317,7 @@ class Routing extends Component {
                                 path='/profile/:accountId'
                                 component={Profile}
                             />
-                            <PrivateRoute
+                            <PrivateRouteLimited
                                 exact
                                 path='/profile/:accountId?'
                                 component={Profile}
